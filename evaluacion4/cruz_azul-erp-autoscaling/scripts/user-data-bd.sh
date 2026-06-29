505462b3ded6b7d6d5c7f4b235416e4400a495f4
Franc-isco <francisco.tabilo05@inacapmail.cl>
2026-06-29 06:30:49 -0400

    Update user-data-bd.sh

--- evaluacion4/cruz_azul-erp-autoscaling/scripts/user-data-bd.sh
+++ evaluacion4/cruz_azul-erp-autoscaling/scripts/user-data-bd.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+
 apt update -y
 apt install -y docker.io docker-compose git
 
 systemctl enable docker
 systemctl start docker
+
+mkdir -p /srv
+
+if [ ! -d "/srv/evaluacion2-cruzazul-erp" ]; then
+  git clone https://github.com/Franc-isco/evaluacion2-cruzazul-erp.git /srv/evaluacion2-cruzazul-erp
+fi
+
+rm -rf /srv/cruz_azul-erp
+cp -r /srv/evaluacion2-cruzazul-erp/evaluacion4/cruz_azul-erp-autoscaling /srv/cruz_azul-erp
 
 cd /srv/cruz_azul-erp/db
 docker-compose -f docker-compose.bd.yml up -d
